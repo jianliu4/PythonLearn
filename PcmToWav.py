8106477b4dd7bd10f3f37673930662bcc9f40655
JianLiu <jianliu4@iflytek.com>
2016-07-02 23:37:30 +0800

    Test speed pcm to wav

--- PcmToWav.py
+++ PcmToWav.py
@@ -3,31 +3,29 @@ import wave
 import random
 import struct
 import datetime
-noise_output = wave.open('noise.wav', 'w')
-noise_output.setparams((1, 2, 16000, 0, 'NONE', 'not compressed'))
-
-d1 = datetime.datetime.now()
-
-sample_len = 1323000
 print ('----第一种方法----')
+noise_file1 = wave.open('noise.wav', 'w')
+noise_file1.setparams((1, 2, 16000, 0, 'NONE', 'not compressed'))
+d1 = datetime.datetime.now()
+sample_len = 1323000
 print ('writing')
 for i in range(0 , sample_len):
     value = random.randint(-32767, 32767)
     packed_vaule = struct.pack('h', value)
-    noise_output.writeframes(packed_vaule)
-noise_output.close()
-
+    noise_file1.writeframes(packed_vaule)
+noise_file1.close()
 d2 = datetime.datetime.now()
 print ('第一种方法所花费的时间：'),(d2-d1)
+
 print ('----第二种方法----')
-noise_file = open('noise.pcm','w')
+noise_file2 = open('noise.pcm','w')
 for i in range(0,sample_len):
     value = random.randint(-32767, 32767)
     packed_vaule = struct.pack('h', value)
-    noise_file.write(packed_vaule)
+    noise_file2.write(packed_vaule)
 d3 = datetime.datetime.now()
 print('第二种方法所花费的时间：'), (d3-d2)
-noise_file.close()
+noise_file2.close()
 
 print ('----第三种方法----')
 noise_file3 = wave.open('noise_file3.wav','w')
